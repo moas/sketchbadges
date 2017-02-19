03db77c85dd18e28d71b77e139a19afa05f27265
Oscar L. aka moas <lasm.landry@gmail.com>
2017-02-19 14:52:04 +0100

    settings refactor

--- exo/config/settings/common.py
+++ exo/config/settings/common.py
@@ -197,24 +197,6 @@ ADMIN_URL = r'^admin/'
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
 
-# CACHING
-# ------------------------------------------------------------------------------
-
-REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 1)
-# Heroku URL does not pass the DB number, so we parse it in
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': REDIS_LOCATION,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            "PASSWORD": "",
-            'IGNORE_EXCEPTIONS': True,  # mimics memcache behavior.
-                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-        }
-    }
-}
-
 DJANGO_REDIS_LOG_IGNORED_EXCEPTIONS = True
 
 # users model
--- exo/config/settings/production.py
+++ exo/config/settings/production.py
@@ -1,1 +1,17 @@
 from .common import *
+
+
+REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 1)
+# Heroku URL does not pass the DB number, so we parse it in
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_LOCATION,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            "PASSWORD": "",
+            'IGNORE_EXCEPTIONS': True,  # mimics memcache behavior.
+                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+        }
+    }
+}
